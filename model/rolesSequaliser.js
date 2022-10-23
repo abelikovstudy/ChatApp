2a845f30d790a028825f2a750b679ba750950970
abelikovstudy <beli@sfedu.ru>
2022-10-23 18:42:48 +0400

    Добавлается работа с БД.

---
+++ model/rolesSequaliser.js
@@ -0,0 +1,69 @@
+const config = require("../config/dbConfig.js");
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(
+  config.DB,
+  config.USER,
+  config.PASSWORD,
+  {
+    host: config.HOST,
+    dialect: config.dialect,
+    operatorsAliases: false,
+
+    pool: {
+      max: config.pool.max,
+      min: config.pool.min,
+      acquire: config.pool.acquire,
+      idle: config.pool.idle
+    }
+  }
+);
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = (sequelize, Sequelize) => {
+    const User = sequelize.define("users", {
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      }
+    });
+  
+    return User;
+  };
+db.role = (sequelize, Sequelize)=> {
+    const Role = sequelize.define("roles", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING
+      }
+    });
+  
+    return Role;
+  };
+
+db.role.belongsToMany(db.user, {
+  through: "user_roles",
+  foreignKey: "roleId",
+  otherKey: "userId"
+});
+db.user.belongsToMany(db.role, {
+  through: "user_roles",
+  foreignKey: "userId",
+  otherKey: "roleId"
+});
+
+db.ROLES = ["user", "admin", "moderator"];
+
+module.exports = db;
