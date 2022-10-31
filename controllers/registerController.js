a897256c890100693ae0184e1b645778bd6e0430
abelikovstudy <beli@sfedu.ru>
2022-10-31 21:22:26 +0300

    Resolved problems with registration

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,45 +1,52 @@
-const userDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-
-const fsPromises = require('fs').promises;
-const path = require('path');
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
 const db = require('../model/rolesSequaliser')
 
 const Roles = db.ROLES;
 const Users = db.user;
+const Op = db.Sequelize.Op;
 
 const handleNewUser = async (req, res) => {
-    const {user, pwd} = req.body;
-    if(!user || !pwd ) return res.status(400).json(
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json(
         {
-            'message' : 'Username and password are required'
+            'message': 'Username and password are required'
         }
     );
+    try {
+        User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 8)
+        })
+            .then(user => {
+                if (req.body.roles) {
+                    Role.findAll({
+                        where: {
+                            name: {
+                                [Op.or]: req.body.roles
+                            }
+                        }
+                    }).then(roles => {
+                        user.setRoles(roles).then(() => {
+                            res.status(200).json({ message: "User was registered successfully!" });
+                        });
+                    });
+                } else {
+                    user.setRoles([1]).then(() => {
+                        res.status(200).json({ message: "User was registered successfully!" });
+                    });
+                }
+            }).catch(err => {
+                res.status(500).send({message : err.message})
+            })
 
-        
-
-    const duplicate = User.findOne()
-    if(duplicate) return res.sendStatus(409);
-    try{
-        const hashedPwd = await bcrypt.hash(pwd, 10);
-        const newUser = {
-            "username" : user,
-            "password" : hashedPwd
-        }
-        userDB.setUsers([...userDB.users, newUser]);
-        await fsPromises.writeFile(path.join(__dirname, '..', 'model', 'users.json'), JSON.stringify(userDB.users));
-        res.status(201).json({
-            'success' : `New user created: ${user}`
-        });
     }
-    catch(err){
+    catch (err) {
         res.status(500).json({
-            'message' : `Server error: ${err.message}`
+            'message': `Server error: ${err.message}`
         })
     }
 }
 
-module.exports = {handleNewUser};
+module.exports = { handleNewUser };
--- controllers/verification/registerVeify.js
+++ controllers/verification/registerVeify.js
@@ -2,13 +2,15 @@ const db = require("../../model/rolesSequaliser");
 const ROLES = db.ROLES;
 const User = db.user;
 
-const checkDuplicate = (req, res , next) =>{
-    User.findOne({where:{
-       username: req.body.user 
-    }}).then(user => {
-        if(user){
+const checkDuplicate = (req, res, next) => {
+    User.findOne({
+        where: {
+            username: req.body.user
+        }
+    }).then(user => {
+        if (user) {
             res.status(400).send({
-                message : "Username already taken!"
+                message: "Username already taken!"
             })
         }
         return;
